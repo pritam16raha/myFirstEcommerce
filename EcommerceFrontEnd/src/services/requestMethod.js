a5b630f5cd6ab130ca0c6a2ec57f6047f70a51f8
PRITAM RAHA <116270642+pritam16raha@users.noreply.github.com>
2024-02-28 17:00:19 +0530

    Update requestMethod.js
    access token and api has been deleted from services

--- EcommerceFrontEnd/src/services/requestMethod.js
+++ EcommerceFrontEnd/src/services/requestMethod.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = ""
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZDQ2NDQ1Y2Y2MDA4ZGJkN2FiZDM4YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcwOTA5MzMzMSwiZXhwIjoxNzA5MzUyNTMxfQ.en9Brc3OLu0tdqyCx_u40njIBKDJmGieaKHEZY4061M"
+const TOKEN = ""
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
